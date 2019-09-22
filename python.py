d05f5c374bb7d5f2a70ca3cf57597eba35a5895d
RkReddy <rkreddii01@gmail.com>
2019-09-22 17:47:25 +0530

    first Commit

---
+++ python.py
@@ -0,0 +1,14 @@
+num1 = int(input())
+num2 = int(input())
+
+if(num1 > num2):
+  
+  print("Fuck off Num2")
+
+elif(num1 < num2):
+
+  print("Fuck you Num1")
+
+else:
+
+  print("Oh Man, Num1&Num2 I will beat you ppl asses like charcoke Drum(BTW it's a popular dialouge from Fast and furious 8)")
